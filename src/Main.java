dd33bda8eebfb18ca12d6b7510a901c2a5ac33d5
Flower <68172262+NoBrain0917@users.noreply.github.com>
2021-03-19 18:57:49 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception{
-        new Painter().run("C:\\Users\\All User\\Desktop\\asdf.jpg");
+        new Painter().run(IMAGE_PATH);
     }
 }
